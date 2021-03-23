d9f2993030e4da25095d33240d980da6e579da06
xianan2 <65153699+xianan2@users.noreply.github.com>
2021-03-23 12:06:04 +0800

    made login and and register page, created buttons to navigate to the 2 pages in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'register_page.dart';
+import 'login_page.dart';
 
 void main() {
   return runApp(
@@ -38,44 +40,64 @@ class _CarparkPageState extends State<CarparkPage> {
           ),
           SizedBox(
               height: 20.0,
-              width:300.0,
+              width: 300.0,
               child: Divider(
                 color: Colors.red,
-              )
-          ),
+              )),
           Card(
-            color:Colors.red,
-            margin: EdgeInsets.symmetric(vertical:10.0, horizontal: 120.0),
-            child: ListTile(
-              leading: Icon(
-                Icons.mail,
-                color: Colors.white,
-              ),
-              title: Text(
-                  'Register',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  )
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 107.0),
+            child: RaisedButton(
+              color: Colors.red,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return RegisterPage();
+                    },
+                  ),
+                );
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.mail,
+                  color: Colors.white,
+                ),
+                title: Text('Register',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    )),
               ),
             ),
           ),
-          Card( //default card color is white
-            color:Colors.red,
-            margin: EdgeInsets.symmetric(vertical:10.0, horizontal: 120.0),
-            child: ListTile(
-              leading: Icon(
-                Icons.login,
-                color: Colors.white,
-              ),
-              title: Text(
-                  'Login',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.bold,
-                  )
+          Card(
+            //default card color is white
+            margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 107.0),
+            child: RaisedButton(
+              color: Colors.red,
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return LoginPage();
+                    },
+                  ),
+                );
+              },
+              child: ListTile(
+                leading: Icon(
+                  Icons.login,
+                  color: Colors.white,
+                ),
+                title: Text('Login',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    )),
               ),
             ),
           ),
@@ -84,7 +106,3 @@ class _CarparkPageState extends State<CarparkPage> {
     );
   }
 }
-
-
-
-
