f5dd3a0479d7f5712ad50ce9aa6972d4fcdbb6a9
xianan2 <65153699+xianan2@users.noreply.github.com>
2021-03-30 02:11:18 +0800

    added function to get daily token, return carpark info object
    need to build a coordinate converter from SVY21(SG) to WGS84 (long lat), probably just gna use one Map API so...

--- lib/services/find_carparks.dart
+++ lib/services/find_carparks.dart
@@ -2,29 +2,67 @@ import 'networking.dart';
 import 'dart:convert';
 import 'location.dart';
 import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
-const apiKey = 'AIzaSyCx8dZkszicSqdq1w2JyFIRYImX-5Pob9A';
+const googleApiKey = 'AIzaSyCx8dZkszicSqdq1w2JyFIRYImX-5Pob9A';
+const uraAccessKey = '62f968e9-3534-4c1c-9250-44e04671037c';
 
 class FindCarpark {
-  Future<dynamic> carparkInformation() async {
-    var dio = Dio();
-    var url =
-        'https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Availability';
-    var parameters = {
-      'AccessKey': '62f968e9-3534-4c1c-9250-44e04671037c',
-      'Token':
-          'WMc-yhdmcBKk0wuSH6R4zsK61YP0K3GnW-B@7af732Zf3xa7F8 9 zXD+29GNec785k4PAnJf6H1cg29s84r9H48e898pgQFYeCc',
-    };
-    var response = await dio.get(url, data: parameters);
-    return response.data;
+  Future<String> getDailyToken() async {
+    final response = await http.get(
+        Uri.https('www.ura.gov.sg', '/uraDataService/insertNewToken.action'),
+        headers: {'AccessKey': uraAccessKey});
+
+    final responseJson = jsonDecode(response.body);
+    final token = responseJson['Result'];
+
+    return token;
   }
+
+  Future<dynamic> carparkInformation() async {
+    final response = await http.get(
+      Uri.https(
+        'www.ura.gov.sg',
+        '/uraDataService/invokeUraDS',
+        {'service': 'Car_Park_Availability'},
+      ),
+      headers: {
+        'AccessKey': uraAccessKey,
+        'Token': await getDailyToken(),
+      },
+    );
+
+    final responseJson = jsonDecode(response.body);
+
+    // For testing the accessing of API
+    // for (var i = 0; i <= 10; i++) {
+    //   String coordinates =
+    //       responseJson['Result'][i]['geometries'][0]['coordinates'];
+    //   print(coordinates);
+    // }
+
+    // Currently returns probably the entire Sg's carpark or some shit
+    return responseJson;
+  }
+  // Future<dynamic> carparkInformation() async {
+  //   var dio = Dio();
+  //   var url =
+  //       'https://www.ura.gov.sg/uraDataService/invokeUraDS?service=Car_Park_Availability';
+  //   var parameters = {
+  //     'AccessKey': '62f968e9-3534-4c1c-9250-44e04671037c',
+  //     'Token':
+  //         'WMc-yhdmcBKk0wuSH6R4zsK61YP0K3GnW-B@7af732Zf3xa7F8 9 zXD+29GNec785k4PAnJf6H1cg29s84r9H48e898pgQFYeCc',
+  //   };
+  //   var response = await dio.get(url, data: parameters);
+  //   return response.data;
+  // }
 
   Future<List<String>> searchNearby(String keyword) async {
     String constant = 'car park near';
     var dio = Dio();
     var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json';
     var parameters = {
-      'key': apiKey,
+      'key': googleApiKey,
       'query': constant + keyword,
       'radius': '4000',
     };
